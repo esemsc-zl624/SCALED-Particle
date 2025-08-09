7844737abcbc902b6b4f2e763e96cf59978f7675
Zhikai <zl624@ic.ac.uk>
2025-08-09 23:31:59 +0800

    update: exp3

---
+++ exp/exp3_complex_model/train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=exp2_bc
+#SBATCH --partition=dgxl_irp
+#SBATCH --qos=dgxl_irp_low
+
+#SBATCH -e exp/exp3_complex_model/logs/%j-exp3_complex_model.err              # File to redirect stderr
+#SBATCH -o exp/exp3_complex_model/logs/%j-exp3_complex_model.out              # File to redirect stdout
+#SBATCH --mem=10GB                   # Memory per processor
+#SBATCH --time=24:00:00              # The walltime
+#SBATCH --nodes=1                    # Run all processes on a single node
+#SBATCH --ntasks-per-node=1          # number of MP tasks
+#SBATCH --cpus-per-task=12           # CPUs per task
+#SBATCH --gres=gpu:2                 # Number of GPUs
+
+
+
+# uv
+# source /scratch_dgxl/zl624/workspace/SCALED-Particle/.venv/bin/activate
+
+# conda
+source /scratch_dgxl/zl624/miniconda3/etc/profile.d/conda.sh && conda activate scaled
+
+accelerate launch tools/trainning/train.py --config exp/exp3_complex_model/config/exp3.yaml
