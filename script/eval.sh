8ba2ddce01f6bdb178cfc8b887dd7797d9a3e1f5
Zhikai Li <zl624@ic.ac.uk>
2025-08-18 10:07:22 +0000

    feat: train and eval script

---
+++ script/eval.sh
@@ -0,0 +1,11 @@
+EXP_DIR=exp/exp4_gtbc
+
+# uv
+# source /scratch_dgxl/zl624/workspace/SCALED-Particle/.venv/bin/activate
+
+# conda
+source /scratch_dgxl/zl624/miniconda3/etc/profile.d/conda.sh && conda activate scaled
+
+# python src/scaled/tools/inference/inference.py --config $EXP_DIR/config/exp4.yaml
+
+python src/scaled/tools/visualization/viz_rollout_comparation.py --pred_input_dir $EXP_DIR/rollout/npy --output_dir $EXP_DIR/rollout
