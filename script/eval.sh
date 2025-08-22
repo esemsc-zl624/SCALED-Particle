e47770ac15d0007eacc82a1f4aa44fd358803c8e
Zhikai Li <zl624@ic.ac.uk>
2025-08-22 02:34:10 +0000

    update: script for evaluation

--- script/eval.sh
+++ script/eval.sh
@@ -6,6 +6,9 @@ EXP_DIR=exp/exp4_gtbc
 # conda
 source /scratch_dgxl/zl624/miniconda3/etc/profile.d/conda.sh && conda activate scaled
 
-# python src/scaled/tools/inference/inference.py --config $EXP_DIR/config/exp4.yaml
+# python src/scaled/tools/inference/inference.py --config $EXP_DIR/config/exp4.yaml --inference_type rollout
+# python src/scaled/tools/inference/inference.py --config $EXP_DIR/config/exp4.yaml --inference_type onestep
 
-python src/scaled/tools/visualization/viz_rollout_comparation.py --pred_input_dir $EXP_DIR/rollout/npy --output_dir $EXP_DIR/rollout
+# python src/scaled/tools/visualization/viz_rollout_comparation.py --pred_input_dir $EXP_DIR/rollout/npy --output_dir $EXP_DIR/rollout
+
+python src/scaled/tools/evaluation/calculate_mse.py --rollout_pred_dir $EXP_DIR/rollout/npy --one_step_pred_dir $EXP_DIR/onestep/npy
