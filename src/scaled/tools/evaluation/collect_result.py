24c2065abdd75087571f5cd8611ff4c34bcedf73
Zhikai <zl624@ic.ac.uk>
2025-08-26 23:46:50 +0800

    update: collect result

--- src/scaled/tools/evaluation/collect_result.py
+++ src/scaled/tools/evaluation/collect_result.py
@@ -1,49 +1,64 @@
 import pandas as pd
 import os
+import argparse
 
-# onestep_results = ["exp/01_conv/onestep/metrics.csv", "exp/02_attn/onestep/metrics.csv", "exp/03_conv_sfc/onestep/metrics.csv", "exp/04_attn_sfc/onestep/metrics.csv"]
-# rollout_results = ["exp/01_conv/rollout/metrics.csv", "exp/02_attn/rollout/metrics.csv", "exp/03_conv_sfc/rollout/metrics.csv", "exp/04_attn_sfc/rollout/metrics.csv"]
-onestep_results = ["exp/01_conv/onestep/metrics.csv", "exp/02_attn/onestep/metrics.csv"]
-rollout_results = ["exp/01_conv/rollout/metrics.csv", "exp/02_attn/rollout/metrics.csv"]
 
-os.makedirs("result", exist_ok=True)
-result_dir = "result/summary.csv"
+def main(args):
 
-# 创建空的DataFrame来存储所有结果
-all_results = []
+    all_results = []
+    onestep_results = []
+    rollout_results = []
 
-for file_path in onestep_results:
-    df = pd.read_csv(file_path)
+    for exp_dir in args.exp_dirs:
+        onestep_results.append(os.path.join(exp_dir, "onestep", "metrics.csv"))
+        rollout_results.append(os.path.join(exp_dir, "rollout", "metrics.csv"))
 
-    result_row = {
-        "mode": "onestep",
-        "file_path": file_path,
-        "recall": df["recall"].mean(),
-        "precision": df["precision"].mean(),
-        "f1_score": df["f1"].mean(),
-        "mse": df["mse"].mean(),
-    }
-    all_results.append(result_row)
+    for file_path in onestep_results:
+        df = pd.read_csv(file_path)
 
-for file_path in rollout_results:
-    df = pd.read_csv(file_path)
+        result_row = {
+            "mode": "onestep",
+            "file_path": file_path,
+            "recall": df["recall"].mean(),
+            "precision": df["precision"].mean(),
+            "f1_score": df["f1"].mean(),
+            "mse": df["mse"].mean(),
+        }
+        all_results.append(result_row)
 
-    result_row = {
-        "mode": "rollout",
-        "file_path": file_path,
-        "recall": df["recall"].mean(),
-        "precision": df["precision"].mean(),
-        "f1_score": df["f1"].mean(),
-        "mse": df["mse"].mean(),
-    }
-    all_results.append(result_row)
+    for file_path in rollout_results:
+        df = pd.read_csv(file_path)
 
-result_df = pd.DataFrame(all_results)
+        result_row = {
+            "mode": "rollout",
+            "file_path": file_path,
+            "recall": df["recall"].mean(),
+            "precision": df["precision"].mean(),
+            "f1_score": df["f1"].mean(),
+            "mse": df["mse"].mean(),
+        }
+        all_results.append(result_row)
 
-numeric_columns = ['recall', 'precision', 'f1_score', 'mse']
-for col in numeric_columns:
-    result_df[col] = result_df[col].apply(lambda x: f"{x:.3e}")
+    result_df = pd.DataFrame(all_results)
 
-result_df.to_csv(result_dir, index=False)
+    numeric_columns = ["recall", "precision", "f1_score", "mse"]
+    for col in numeric_columns:
+        result_df[col] = result_df[col].apply(lambda x: f"{x:.3e}")
 
-print(result_df)
+    result_df.to_csv(args.result_save_path, index=False)
+
+    print(result_df)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--exp_dirs",
+        default=["exp/01_conv", "exp/02_attn", "exp/03_conv_sfc", "exp/04_attn_sfc"],
+    )
+    parser.add_argument("--result_save_path", default="result/summary.csv")
+
+    args = parser.parse_args()
+    os.makedirs(os.path.dirname(args.result_save_path), exist_ok=True)
+
+    main(args)
