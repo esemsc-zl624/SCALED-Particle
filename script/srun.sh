e3ebd43653880b8e536f326e5e653daf2f784a77
Zhikai <zl624@ic.ac.uk>
2025-08-29 13:06:35 +0800

    update: delete useless script

--- script/srun.sh
+++
@@ -1,7 +0,0 @@
-srun --pty -p dgxl_irp --gres=gpu:1 --cpus-per-task=12 --mem=16G --qos=dgxl_irp_low bash
-
-# uv
-# source /scratch_dgxl/zl624/workspace/SCALED-Particle/.venv/bin/activate
-
-# conda
-source /scratch_dgxl/zl624/miniconda3/etc/profile.d/conda.sh  && conda activate scaled
